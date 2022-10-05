9ebc6f3b0a7564b2f1de5bba2dcc5731d0e21b4f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-10-05 15:42:17 +0700

    create react app

---
+++ src/components/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const MainPage = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default MainPage;
